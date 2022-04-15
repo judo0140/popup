7fcb89f2a30c9196bc8ef58dcd8bb4ca5dc7bcba
judo0140 <61779427+judo0140@users.noreply.github.com>
2022-04-15 10:11:47 +0900

    Update popup_dxl_v2.py

--- popup_pi_1/popup_dxl_v2.py
+++ popup_pi_1/popup_dxl_v2.py
@@ -7,6 +7,13 @@ from std_msgs.msg import String
 
 import dxl
 
+# class 설명
+# dxl.bringup(dxl_id) : 다이나믹셀 구동 위한 bringup class .. ex) dxl.bringup(1) = ID:1 인 다이나믹셀 구동 준비
+# dxl.setting(dxl_id, velocity, acceleration) : 다이나믹셀 구동 회전 속도, 가속도 설정 .. ex) dxl.bringup(1, 50, 20) = ID:1 인 다이나믹셀 속도 50, 가속도 20으로 설정
+# dxl.goal(dxl_id, goal_psition) : 다이나믹셀에 목표 위치 명령 .. ex) dxl.goal(1, 3000) = ID:1 인 다이나믹셀을 위치값 3000인 위치로 회전
+# dxl.stop(dxl_id) : 다이나믹셀 구동 중지 .. ex) dxl.stop(1) = ID:1 인 다이나믹셀 구동 중지
+# dxl.read(dxl_id).call() : 다이나믹셀 현재 위치값 읽어오기 .. ex) dxl.read(1).call() = ID:1 인 다이나믹셀 현재 위치값 읽어오기
+
 # dxl1, dxl2 = sidetable
 dxl.bringup(1)
 dxl.setting(1, 80, 20)
@@ -31,12 +38,15 @@ def listener():
 
 def callback(data):
     rospy.loginfo(data.data)
+    
     # topic = 'bed:sidetable:chair:desk' ex) '0.5:0:1:1'
     topic = data.data
+    
     # parsing topic ex) '0.5','0','1','1'
     strings = topic.split(':')
     bed_mode = strings[0]
     sidetable_mode = strings[1]
+    
     if bed_mode == 'b0':
         dxl.goal(3, init_dxl3-16000)
         dxl.goal(4, init_dxl4+16000)
@@ -55,5 +65,3 @@ def callback(data):
 
 if __name__ == '__main__':
     listener()
-    #dxl.goal(1, init_dxl1 - 10000)
-    #dxl.goal(2, init_dxl2 + 10000)
